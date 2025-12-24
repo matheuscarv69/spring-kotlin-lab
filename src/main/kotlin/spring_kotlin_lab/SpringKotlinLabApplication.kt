f6282f1dad8d4e7b0f76ad48d4620b5cee03c86c
matheuscarv69 <matheus9126@gmail.com>
2025-12-24 19:40:23 -0400

    :tada: initial commit

---
+++ src/main/kotlin/spring_kotlin_lab/SpringKotlinLabApplication.kt
@@ -0,0 +1,11 @@
+package spring_kotlin_lab
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinLabApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringKotlinLabApplication>(*args)
+}
---
+++ src/test/kotlin/spring_kotlin_lab/SpringKotlinLabApplicationTests.kt
@@ -0,0 +1,13 @@
+package spring_kotlin_lab
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinLabApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
